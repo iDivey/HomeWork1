9e42ea664f580574e74213638682b8148b345d23
iDivey <150763080+iDivey@users.noreply.github.com>
2024-03-11 11:02:44 +0700

    Неизменяемые и изменяемые объекты

---
+++ homework4.py
@@ -0,0 +1,6 @@
+immutable_var = 2024, False, 'KARTEG'
+print(immutable_var)
+#immutable_var[1] = True #Кортеж является неизменямым объектом
+mutable_list = [11, True, 'Spisok']
+mutable_list[2] = 'List'
+print(mutable_list)
