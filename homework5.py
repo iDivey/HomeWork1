4221b783c104ccee2b41ba13788bf40f83a723af
iDivey <150763080+iDivey@users.noreply.github.com>
2024-03-12 10:33:41 +0700

    Списки и словари

---
+++ homework5.py
@@ -0,0 +1,12 @@
+my_list = ['apple', 'mango', 'tangerin', 'banana', 'pineapple']
+print(my_list)
+print(my_list[0:6:4])
+print(my_list[2:6])
+my_list[2] = 'mandarin'
+print(my_list)
+# словарь
+my_dict = {'apple': 'яблоко', 'mango': 'манго', 'tangerin': 'мандарин', 'banana': 'банан', 'pineapple': 'ананас'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['tangerin'] = 'танджерин'
+print(my_dict)
