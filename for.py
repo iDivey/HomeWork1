f40fe021ef4f1bd1a4f66eb66a7353a06731b5f7
iDivey <150763080+iDivey@users.noreply.github.com>
2024-03-18 12:01:36 +0700

    Цикл for. Элементы списка

---
+++ for.py
@@ -0,0 +1,8 @@
+list_ = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+for i in range(len(list_)):
+    print(f'Я езжу на автомабиле марки {list_[i]}')
+
+cars_count = 0
+while True:
+    cars_count += 10
+    print(cars_count)
