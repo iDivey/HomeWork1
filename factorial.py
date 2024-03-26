927f33edd92143b066172129ecaaf03f08811432
iDivey <150763080+iDivey@users.noreply.github.com>
2024-03-26 10:55:02 +0700

    Произвольное число параметров, рекурсии, факториал

---
+++ factorial.py
@@ -0,0 +1,15 @@
+def test(*args):
+    print(args)
+
+
+test('arguments', 2, 34, 656, 'кварги')
+
+
+def factorial (n):
+    if n == 1:
+        return 1
+    factorial_n_minus_one = factorial(n=n - 1)
+    return n * factorial_n_minus_one
+
+
+print(factorial(15))
