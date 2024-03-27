ffd0152c95892fc63e6f3999c1a17a46a41fef69
iDivey <150763080+iDivey@users.noreply.github.com>
2024-03-27 11:40:40 +0700

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -6,9 +6,7 @@ test('arguments', 2, 34, 656, 'кварги')
 
 
 def factorial(n):
-    if n == 0:
-        return 1
-    if n == 1:
+    if n == 1 or n == 0:
         return 1
     factorial_n_minus_one = factorial(n=n - 1)
     return n * factorial_n_minus_one
