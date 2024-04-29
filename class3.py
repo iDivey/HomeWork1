19e376dd6ab462859551b506dba779290635e3cb
iDivey <150763080+iDivey@users.noreply.github.com>
2024-04-29 08:59:17 +0700

    Update class3.py

--- class3.py
+++ class3.py
@@ -1,20 +1,14 @@
 class Building:
-    def __init__(self):
-        self.numberOfFloors = 5
-        self.buildingType = 'Хрущёвка'
+    def __init__(self, numberOfFloors: int, buildingType: str):
+        self.numberOfFloors = numberOfFloors
+        self.buildingType = buildingType
 
     def __eq__(self, other):
         return self.numberOfFloors == other.numberOfFloors and self.buildingType == other.buildingType
 
 
-hrushevka = Building()
-neboskreb = Building()
-if hrushevka == neboskreb:
-    print('Допустим')
-else:
-    print('Поверим')
-neboskreb.numberOfFloors = 1000
-neboskreb.buildingType = 'Небоскреб'
+hrushevka = Building(5, 'Хрущевка')
+neboskreb = Building(1000, 'Небоскреб')
 if hrushevka == neboskreb:
     print('Допустим')
 else:
