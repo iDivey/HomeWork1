9f74627de13adc8dbb7630a06da88d2acae429a2
iDivey <150763080+iDivey@users.noreply.github.com>
2024-08-06 11:26:59 +0700

    Введение в функциональное программирование

---
+++ module_9_1.py
@@ -0,0 +1,13 @@
+def apply_all_func(int_list, *functions):
+    results = {}
+    for i in functions:
+        r = i(int_list)
+        results[i.__name__] = r
+
+    return results
+
+
+print(apply_all_func([6, 20, 15, 9], max, min))
+print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+
+
