c0effe8a1f837f2e6dd2a33c50eafba53e17ffc5
iDivey <150763080+iDivey@users.noreply.github.com>
2024-09-11 17:00:12 +0700

    Создание БД, добавление, выбор, удаление элементов

---
+++ module_14_1.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+connection = sqlite3.connect('database.db')
+cursor = connection.cursor()
+
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS Users(
+id INTEGER PRIMARY KEY,
+username TEXT NOT NULL,
+email TEXT NOT NULL,
+age INTEGER,
+balance INTEGER NOT NULL
+)
+''')
+
+cursor.execute('CREATE INDEX IF NOT EXISTS idx_email ON Users (email)')
+
+# - комментировал при новых запусках
+for i in range(1, 11):
+    cursor.execute('INSERT INTO Users (username, email, age, balance) VALUES (?, ?, ?, ?)', (f'User{i}', f'example{i}@gmail.com', f'{i * 10}', f'{1000}'))
+
+# - комментировал при новых запусках
+for i in range(1, 11, 2):
+    cursor.execute('UPDATE Users SET balance = ? WHERE id = ?', (500, f'{i}'))
+
+for i in range(1, 11, 3):
+    cursor.execute('DELETE FROM Users WHERE id = ?', (f'{i}',))
+
+cursor.execute('SELECT username, email, age FROM Users WHERE age != ?', (60,))
+
+users = cursor.fetchall()
+for user in users:
+    print(user)
+
+
+connection.commit()
+connection.close()
