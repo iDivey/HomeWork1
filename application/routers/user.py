b4e36de461db4d73e6d56957f183307b6621d2cf
iDivey <150763080+iDivey@users.noreply.github.com>
2024-09-25 16:50:13 +0700

    Использование БД в маршрутизации. Часть 2

--- application/routers/user.py
+++ application/routers/user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from application.backend.db_depends import get_db
 from typing import Annotated
-from application.models import User
+from application.models import User, Task
 from application.shemas import CreateUser, UpdateUser
 from sqlalchemy import insert, select, update, delete
 from slugify import slugify
@@ -11,19 +11,30 @@ from slugify import slugify
 router = APIRouter(prefix='/user', tags=['user'])
 
 
-@router.get('/')
-async def all_users(db: Annotated[Session, Depends(get_db)]):
-    users = db.scalars(select(User)).all()
-    return users
+@router.get('/user_id/tasks')
+async def tasks_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user = db.scalars(select(User).where(User.id == user_id))
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+
+    tasks_user = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+    return tasks_user
 
 
 @router.get('/user_id')
 async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
     user = db.scalars(select(User).where(User.id == user_id))
+    print(user)
     if user is None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
     else:
         return user
+
+
+@router.get('/')
+async def all_users(db: Annotated[Session, Depends(get_db)]):
+    users = db.scalars(select(User)).all()
+    return users
 
 
 @router.post('/create')
@@ -62,6 +73,7 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
 
     db.execute(delete(User).where(User.id == user_id))
+    db.execute(delete(Task).where(Task.user_id == user_id))
     db.commit()
     return {
         'statys_code': status.HTTP_200_OK,
