d345a3ad27ad36c6955084352be7721c049995d0
iDivey <150763080+iDivey@users.noreply.github.com>
2024-09-28 00:28:50 +0700

    Использование БД в маршрутизации. Часть 2. UPD

--- application/routers/task.py
+++ application/routers/task.py
@@ -1,28 +1,68 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, status, HTTPException
+from sqlalchemy.orm import Session
+from application.backend.db_depends import get_db
+from typing import Annotated
+from application.models import Task, User
+from application.shemas import CreateTask, UpdateTask
+from sqlalchemy import insert, select, update, delete
+from slugify import slugify
 
 router = APIRouter(prefix='/task', tags=['task'])
 
 
 @router.get('/')
-async def all_tasks():
-    return
+async def all_tasks(db: Annotated[Session, Depends(get_db)]):
+    tasks = db.scalars(select(Task)).all()
+    return tasks
 
 
 @router.get('/task_id')
-async def task_by_id():
-    return
+async def task_by_id(db: Annotated[Session, Depends(get_db)], task_id: int):
+    task = db.scalars(select(Task).where(Task.id == task_id)).one_or_none()
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    return task
 
 
 @router.post('/create')
-async def create_task():
-    return
+async def create_task(db: Annotated[Session, Depends(get_db)], create: CreateTask, user_id: int):
+    user = db.scalars(select(User).where(User.id == user_id)).one_or_none()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    db.execute(insert(Task).values(title=create.title, content=create.content,
+                                   priority=create.priority, user_id=user_id,
+                                   slug=slugify(create.title)))
+    db.commit()
+    return {
+        'status_code': status.HTTP_201_CREATED,
+        'transaction': 'Successful'
+    }
+
 
 
 @router.put('/update')
-async def update_task():
-    return
+async def update_task(db: Annotated[Session, Depends(get_db)], task_id: int, up_task: UpdateTask):
+    task = db.scalars(select(Task).where(Task.id == task_id)).one_or_none()
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    db.execute(update(Task).where(Task.id == task_id).values(
+        title=up_task.title, content=up_task.content,
+        priority=up_task.priority))
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'User update is successful!'
+    }
 
 
 @router.delete('/delete')
-async def delete_task():
-    return
+async def delete_task(db: Annotated[Session, Depends(get_db)], task_id: int):
+    task = db.scalars(select(Task).where(Task.id == task_id)).one_or_none()
+    if task is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    db.execute(delete(Task).where(Task.id == task_id))
+    db.commit()
+    return {
+        'status_code': status.HTTP_200_OK,
+        'transaction': 'User update is successful!'
+    }
--- application/routers/user.py
+++ application/routers/user.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status, HTTPException
 from sqlalchemy.orm import Session
 from application.backend.db_depends import get_db
 from typing import Annotated
-from application.models import User
+from application.models import User, Task
 from application.shemas import CreateUser, UpdateUser
 from sqlalchemy import insert, select, update, delete
 from slugify import slugify
@@ -11,10 +11,13 @@ from slugify import slugify
 router = APIRouter(prefix='/user', tags=['user'])
 
 
-@router.get('/')
-async def all_users(db: Annotated[Session, Depends(get_db)]):
-    users = db.scalars(select(User)).all()
-    return users
+@router.get('/user_id/tasks')
+async def tasks_by_user_id(db: Annotated[Session, Depends(get_db)], user_id: int):
+    user = db.scalars(select(User).where(User.id == user_id)).one_or_none()
+    if user is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
+    tasks_user = db.scalars(select(Task).where(Task.user_id == user_id)).all()
+    return tasks_user
 
 
 @router.get('/user_id')
@@ -24,6 +27,12 @@ async def user_by_id(db: Annotated[Session, Depends(get_db)], user_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
     else:
         return user
+
+
+@router.get('/')
+async def all_users(db: Annotated[Session, Depends(get_db)]):
+    users = db.scalars(select(User)).all()
+    return users
 
 
 @router.post('/create')
@@ -62,6 +71,7 @@ async def delete_user(db: Annotated[Session, Depends(get_db)], user_id: int):
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
 
     db.execute(delete(User).where(User.id == user_id))
+    db.execute(delete(Task).where(Task.user_id == user_id))
     db.commit()
     return {
         'status_code': status.HTTP_200_OK,
