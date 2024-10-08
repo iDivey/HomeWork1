5c1e51b57b3a20f67f8fd24d9e641cfdc13555d9
iDivey <150763080+iDivey@users.noreply.github.com>
2024-10-08 12:52:00 +0700

    UPD. Интроспекция

--- module_11_3.py
+++ module_11_3.py
@@ -1,3 +1,6 @@
+import unittest
+
+
 def introspection_info(obj):
     introspect = dict()
     introspect['type'] = type(obj)
@@ -5,7 +8,7 @@ def introspection_info(obj):
     list_method = [attr for attr in dir(obj) if callable(getattr(obj, attr))]
     introspect['attributes'] = list_attr
     introspect['methods'] = list_method
-    introspect['module'] = obj.__module__
+    introspect['module'] = getattr(obj, '__module__', None)
     return introspect
 
 
@@ -21,6 +24,4 @@ class Figura:
 
 S = Figura(5, 6)
 print(introspection_info(S))
-
-
-
+print(introspection_info(42))
